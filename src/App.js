e0b6a57ebf4e82d8590b39b8a05a115f7c101c28
Omeje Emmanuel Xavier <omejeemmanuel046@yahoo.com>
2022-07-04 13:43:49 +0100

    Omeje world

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import './App.css';
-import Navbar from './components/header/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/pages/homePage/Home';
-import Footer from './components/footer.js/Footer';
+import React from 'react';
+import './App.css'
 
-const App = () => {
+function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path='/' exact element={<Home />} />
-      </Routes>
-      <Footer />
-    </Router>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
